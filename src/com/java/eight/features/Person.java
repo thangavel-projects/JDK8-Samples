69b2ef7dfb594c3b1cd3914f516d81259387a68a
Thangavel Loganathan <Thangavel_Loganathan@epam.com>
2016-12-01 11:53:34 +0100

    JDK8 Samples

---
+++ src/com/java/eight/features/Person.java
@@ -0,0 +1,13 @@
+package com.java.eight.features;
+
+public class Person {
+
+	String firstName;
+	String lastName;
+	
+	public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+}
